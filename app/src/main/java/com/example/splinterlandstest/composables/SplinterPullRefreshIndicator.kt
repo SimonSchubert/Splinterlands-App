aada1444e429a3f0a14690e7c47511a2c004fb3c
SimonSchubert <simon@librem.one>
2022-11-20 19:29:54 +0100

    Add screenshots

---
+++ app/src/main/java/com/example/splinterlandstest/composables/SplinterPullRefreshIndicator.kt
@@ -0,0 +1,28 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
+package com.example.splinterlandstest.composables
+
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material.pullrefresh.pullRefreshIndicatorTransform
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.splinterlandstest.R
+
+@Composable
+fun BoxScope.SplinterPullRefreshIndicator(pullRefreshState: PullRefreshState, hide: Boolean = false) {
+
+    AsyncImage(
+        modifier = Modifier
+            .width(50.dp)
+            .align(Alignment.TopCenter)
+            .pullRefreshIndicatorTransform(pullRefreshState),
+        model = R.drawable.loading,
+        contentDescription = "LOADING"
+    )
+}
