4a458f36c8ff5f0b2df40d2599fde8deec0fde0e
SimonSchubert <simon@librem.one>
2023-04-22 12:01:22 +0200

    Upgrade sdks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.splintergod.app"
         minSdk = 24
         targetSdk = 33
-        versionCode = 14
-        versionName = "0.0.14"
+        versionCode = 15
+        versionName = "0.0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.5"
+        kotlinCompilerExtensionVersion = "1.4.6"
     }
     lint {
         abortOnError = false
@@ -46,9 +46,9 @@ android {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:2.2.4")
-    implementation("io.ktor:ktor-client-cio:2.2.4")
-    implementation("io.ktor:ktor-client-android:2.2.4")
+    implementation("io.ktor:ktor-client-core:2.3.0")
+    implementation("io.ktor:ktor-client-cio:2.3.0")
+    implementation("io.ktor:ktor-client-android:2.3.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
 
@@ -57,10 +57,10 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.compose.material:material:1.4.1")
-    implementation("androidx.compose.animation:animation:1.4.1")
-    implementation("androidx.compose.ui:ui-tooling:1.4.1")
+    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.compose.material:material:1.4.2")
+    implementation("androidx.compose.animation:animation:1.4.2")
+    implementation("androidx.compose.ui:ui-tooling:1.4.2")
     implementation("androidx.navigation:navigation-compose:2.5.3")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
@@ -72,12 +72,12 @@ dependencies {
     implementation("io.insert-koin:koin-core:3.4.0")
     implementation("io.insert-koin:koin-android:3.4.0")
 
-    androidTestImplementation("io.ktor:ktor-client-mock:2.2.4")
+    androidTestImplementation("io.ktor:ktor-client-mock:2.3.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.2")
 }
